9a617d0e3f40cee34d37217a8456ffad842a7462
Daniel Maldonado <danmax2006@gmail.com>
2019-06-02 11:05:09 -0400

    Update code

---
+++ PythonIsEasy/Homework/SongPicker.py
@@ -0,0 +1,29 @@
+# Python is Easy - Homework 1
+''' My Favorite Song
+Hope in Fron of me '''
+
+Artist = "Danny Gokey"
+Album = "Hope in Front of Me"
+Title = "Hope in Front of Me"
+Genre = "Christian/Gospel"
+Duration = 208
+YearRelease = 2014
+BPM = 156
+Producer = "Bernie Herms"
+Writer = ["Danny Gokey", "Bernie Herms", "Brett James"]
+VideoURL = "https://www.youtube.com/watch?v=O5GFiDdGGGM"
+
+SongChorus = "There's hope in front of me There's a light, I still see it There's a hand still holding me Even when I don't believe it"
+AlbumCover = "https://upload.wikimedia.org/wikipedia/en/thumb/e/ee/HopeinFrontofMe.jpg/220px-HopeinFrontofMe.jpg"
+
+ArtistImage = "https://peopledotcom.files.wordpress.com/2019/04/shot_06_227.jpg"
+
+Billboard = "The album debuted at No. 1 on the Billboard Christian Albums chart."
+
+print(Artist, "Is My Favorite", Genre, "artist")
+print("He relased his 2nd Album", Album, "In", YearRelease, Billboard)
+print("My Favorite song on the album is", Title)
+print("Produced by", Producer, "and one of the writers was", Writer [0] )
+
+def SongPicker (song):
+    
